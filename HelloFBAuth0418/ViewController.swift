7248491c53a1ed28c20be97ccbfc5ab694b21233
Danny Shen <shenfive@gmail.com>
2020-04-18 16:02:49 +0800

    FBLogin

--- HelloFBAuth0418/ViewController.swift
+++ HelloFBAuth0418/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController,LoginButtonDelegate {
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let loginButton = FBLoginButton()
+        // Optional: Place the button in the center of your view.
+        loginButton.center = view.center
+        
+        
+//        loginButton.permissions = ["public_profile","email","user_friends"]
+        loginButton.delegate = self
+        
+        
+        
+        view.addSubview(loginButton)
     }
 
+    //MARK:FBLogin Delegate
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        print("FBLogin")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("FBLogout")
+    }
 
 }
 
